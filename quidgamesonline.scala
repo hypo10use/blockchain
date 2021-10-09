aa6618b1600d4834336f0bbd0a40736ccb59208c
Thales <49370370+Hadrianutherae@users.noreply.github.com>
2021-10-09 22:02:00 +0200

    last state

--- quidgamesonline.scala
+++ quidgamesonline.scala
@@ -2,9 +2,12 @@
 import org.ergoplatform.compiler.ErgoScalaCompiler._
 import org.ergoplatform.playgroundenv.utils.ErgoScriptCompiler
 import org.ergoplatform.playground._
+import java.math.BigInteger
 import org.ergoplatform.Pay2SAddress
 import sigmastate.eval.Extensions._
+import scorex.crypto.hash.Digest32
 import scorex.crypto.hash.{Blake2b256}
+import special.collection.Coll
 
 ///////////////////////////////////////////////////////////////////////////////////
 // Create Pin Lock Contract //
@@ -14,6 +17,163 @@ import scorex.crypto.hash.{Blake2b256}
 // To withdraw user must submit the Pin number which gets posted
 // in R4 of the output box as cleartext and hashed to check against
 // the stored hash in the input box R4.
+
+val winnerScript =
+  s"""{
+     |  sigmaProp(
+     |    allOf(Coll(
+     |          // Valid Ticket
+     |          INPUTS(1).tokens(0)._1 == SELF.tokens(0)._1,
+     |          INPUTS(1).R4[Long].get <= SELF.R4[Long].get,
+     |          INPUTS(1).R4[Long].get + INPUTS(1).R5[Long].get > SELF.R4[Long].get
+     |    ))
+     |  )
+     |}""".stripMargin
+
+val TicketScript =
+  s"""{
+     |  val winnerPhaseSpend = HEIGHT > deadlineHeight &&
+     |                         1 == 1 &&
+     |                         INPUTS(0).tokens(0)._1 == SELF.tokens(0)._1
+     |
+     |  val receiverCheckWinner = OUTPUTS(0).propositionBytes == SELF.R7[Coll[Byte]].get &&
+     |                            OUTPUTS(0).value == INPUTS(0).value
+     |
+     |  sigmaProp(receiverCheckWinner && winnerPhaseSpend)
+     |}""".stripMargin
+
+val scriptTokenRepo =
+      s"""{
+         |  val totalSoldTicket = SELF.R4[Long].get
+         |  val totalSoldTicketBI: BigInt = totalSoldTicket.toBigInt
+         |  val totalRaised = totalSoldTicket * ticketPrice
+         |  sigmaProp(
+         |    if (HEIGHT < deadlineHeight) {
+         |      allOf(Coll(
+         |            // validate Script
+         |            OUTPUTS(0).propositionBytes == SELF.propositionBytes,
+         |            OUTPUTS(1).R6[Coll[Byte]].get == blake2b256(SELF.propositionBytes),
+         |            // minERG
+         |            INPUTS(1).value >= ticketPrice + 2 * minFee,
+         |            // validate Register
+         |            OUTPUTS(0).R4[Long].get == totalSoldTicket + (INPUTS(1).value - 2 * minFee) / ticketPrice,
+         |            OUTPUTS(1).R4[Long].get == totalSoldTicket,
+         |            OUTPUTS(1).R5[Long].get == (INPUTS(1).value - 2 * minFee) / ticketPrice,
+         |            // validate Token
+         |            OUTPUTS(0).tokens(0)._1 == SELF.tokens(0)._1,
+         |            OUTPUTS(0).tokens(0)._2 == SELF.tokens(0)._2 - (INPUTS(1).value - 2 * minFee) / ticketPrice,
+         |            OUTPUTS(0).tokens(1)._1 == SELF.tokens(1)._1, // Raffle Service Token
+         |            OUTPUTS(1).tokens(0)._1 == SELF.tokens(0)._1,
+         |            OUTPUTS(1).tokens(0)._2 == (INPUTS(1).value - 2 * minFee) / ticketPrice,
+         |            // ERG Protect
+         |            OUTPUTS(0).value == SELF.value + INPUTS(1).value - 2 * minFee,
+         |            OUTPUTS(1).value == minFee,
+         |            ))
+         |    }
+         |    else {
+         |        allOf(Coll(
+         |              // Validate Size
+         |              INPUTS.size == 1 && OUTPUTS.size == 5,
+         |              // Winner Box
+         |              OUTPUTS(0).value  >= totalRaised,
+         |              OUTPUTS(0).R4[Long].get == ((byteArrayToBigInt(CONTEXT.dataInputs(0).id.slice(0, 15)).toBigInt + totalSoldTicketBI) % totalSoldTicketBI).toBigInt,
+         |              OUTPUTS(0).tokens(0)._1 == SELF.tokens(0)._1,
+         |              OUTPUTS(0).tokens(0)._2 == SELF.tokens(0)._2
+         |         ))
+         |     
+         |  })
+         |}""".stripMargin
+
+val blockchainSim = newBlockChainSimulationScenario("Quid Games")
+val pinNumber = "1293"
+val userParty = blockchainSim.newParty("Abdi")
+val winnerContract = ErgoScriptCompiler.compile(
+Map(), winnerScript)
+
+val newRound = blockchainSim.newParty("Round 1")
+
+val ticketContract = ErgoScriptCompiler.compile(
+Map("deadlineHeight" -> 50, 
+   "ticketPrice" -> 1000000L,
+   "projectPubKey" -> newRound.wallet.getAddress.pubKey),
+TicketScript)
+
+val raffleTokenId: String = "298cbf467b7c5fd38fd3dd8cea35d6c3911f6960db6f6a66548f242a41742870"
+val raffleContract = ErgoScriptCompiler.compile(
+  Map(
+   "ticketPrice" -> 1000000L,
+      "minToRaise" -> 600000000000L,
+      "deadlineHeight" -> 50000000,
+      "minFee" -> 1000000L),
+  scriptTokenRepo)
+
+val quidToken = blockchainSim.newToken("QUID")
+val quidTokenAmount = 40L
+
+userParty.generateUnspentBoxes(
+      toSpend       = 1000000000,
+      tokensToSpend = List(quidToken -> quidTokenAmount))
+
+val ticketToken = blockchainSim.newToken("TICKET")
+
+val gameBox = Box(value = 1000000L,
+                          script = raffleContract,
+                        token = (quidToken -> quidTokenAmount),
+                          registers = Map(R4 -> pinNumber.getBytes(), R5 -> pinNumber.getBytes(), R6 -> pinNumber.getBytes()))
+
+
+val participantBox = Box(value = 4000000L,
+                          script = contract(userParty.wallet.getAddress.pubKey))
+
+val generateGameBox = Transaction(
+      inputs       = userParty.selectUnspentBoxes(toSpend = 1000000L),
+      outputs      = List(gameBox),
+      fee          = MinTxFee,
+      sendChangeTo = userParty.wallet.getAddress
+    )
+
+userParty.printUnspentAssets()
+println("-----------")
+
+val generateGameBoxSigned = userParty.wallet.sign(generateGameBox)
+blockchainSim.send(generateGameBoxSigned)
+
+val generateParticipantBox = Transaction(
+      inputs       = userParty.selectUnspentBoxes(toSpend = 4000000L),
+      outputs      = List(participantBox),
+      fee          = MinTxFee,
+      sendChangeTo = userParty.wallet.getAddress
+    )
+
+val generateParticipantBoxSigned = userParty.wallet.sign(generateParticipantBox)
+blockchainSim.send(generateParticipantBoxSigned)
+
+val newGameBox = Box(value = 3000000L,
+                    script = raffleContract,
+                     token = (quidToken -> quidTokenAmount),
+                     registers = Map(R4 -> pinNumber.getBytes(), R5 -> pinNumber.getBytes(), R6 -> pinNumber.getBytes())
+                    )
+
+val ticket = Box(value = 1000000L,
+                script = ticketContract,
+                registers = Map(R4 -> pinNumber.getBytes(), R5 -> pinNumber.getBytes(), R6 -> pinNumber.getBytes(), R7 -> pinNumber.getBytes()),
+                 token = (quidToken -> 20)
+                )
+
+
+
+val purchaseTransaction = Transaction(
+      inputs       = List(generateGameBoxSigned.outputs(0), generateParticipantBoxSigned.outputs(0)),
+      outputs      = List(newGameBox, ticket),
+      fee          = MinTxFee,
+      sendChangeTo = userParty.wallet.getAddress
+    )
+
+
+val purchaseTransactionSigned = userParty.wallet.sign(purchaseTransaction)
+blockchainSim.send(purchaseTransactionSigned)
+
+
 val pinLockScript = s"""
   sigmaProp(INPUTS(0).R4[Coll[Byte]].get == blake2b256(OUTPUTS(0).R4[Coll[Byte]].get))
 """.stripMargin
@@ -28,21 +188,14 @@ println("-----------")
 
 
 
-///////////////////////////////////////////////////////////////////////////////////
-// Prepare A Test Scenario //
-///////////////////////////////////////////////////////////////////////////////////
-// Create a simulated blockchain (aka "Mockchain")
-val blockchainSim = newBlockChainSimulationScenario("PinLock Scenario")
-// Define an actor/user (with a wallet tied to said Party)
-val userParty = blockchainSim.newParty("buyer")
+
 // Define example user input
-val pinNumber = "1293"
+
 // Hash the pin number
 val hashedPin = Blake2b256(pinNumber.getBytes())
 // Define initial nanoErgs in the user's wallet
 val userFunds = 100000000
 // Generate initial userFunds in the user's wallet
-userParty.generateUnspentBoxes(toSpend = userFunds)
 userParty.printUnspentAssets()
 println("-----------")
 
