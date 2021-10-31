988523acd3fb02fd30e2477efd45781164676e0b
Thales <oy532@york.ac.uk>
2021-10-31 19:52:35 +0100

    rwsr

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -44,7 +44,7 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
     BadRequest(s"""{"success": false, "message": "${e.getMessage}"}""").as("application/json")
   }
 
-  def bet(address:String, bet: Int, guess: Int): UnsignedTransaction =  {
+  def bet(address:String, guess: Int): UnsignedTransaction =  {
 
     val client = RestApiErgoClient.create("http://78.46.251.133:9052/swagger", NetworkType.TESTNET, " ")
 
