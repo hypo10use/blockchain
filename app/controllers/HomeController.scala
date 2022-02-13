e237f6de253c217810d27838ddb97525b9b4c358
Thales <oy532@york.ac.uk>
2022-02-13 14:20:21 +0100

    Update HomeController.scala

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -48,7 +48,7 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
 
   def bet(address:String, guess: Int) =  Action {  implicit request: Request[AnyContent] =>
     val organizerAddr = "9fm2q6fv6nyQxPpkd6n111xjt9hGdeMCmTM74W5VfyDZ81EuKmf"
-    val client = RestApiErgoClient.create("http://116.203.30.147:9053/swagger", NetworkType.MAINNET, "")
+    val client = RestApiErgoClient.create("http://135.181.205.79:9053/swagger", NetworkType.MAINNET, "")
     val accessLogger: Logger = Logger("access")
     accessLogger.debug(client.toString())
       client.execute(ctx => {
